81608040aaaaf05d738088fe9c7d944e921414ce
Leonardo Omaña <leotangram@gmail.com>
2021-05-21 15:42:56 -0500

    Refactor rows per pages test

---
+++ src/__fixtures__/handlers.js
@@ -0,0 +1,16 @@
+import { OK_STATUS } from '../consts'
+import { getReposPerPage, makeFakeResponse } from './repos'
+
+export const handlerPaginated = (req, res, ctx) =>
+  res(
+    ctx.status(OK_STATUS),
+    ctx.json({
+      ...makeFakeResponse(),
+      items: getReposPerPage({
+        perPage: Number(req.url.searchParams.get('per_page')),
+        currentPage: req.url.searchParams.get('page'),
+      }),
+    }),
+  )
+
+export default { handlerPaginated }
