942fe207cf03e94592d34a090f6c826edee18422
Leonardo Omaña <leotangram@gmail.com>
2021-05-21 15:07:09 -0500

    Test funcionality rows per page

--- src/__fixtures__/repos.js
+++ src/__fixtures__/repos.js
@@ -1,3 +1,6 @@
+import repos30Paginated from './repos-30-paginated.json'
+import repos50Paginated from './repos-50-paginated.json'
+
 export const makeFakeResponse = ({ totalCount = 0 } = {}) => ({
   total_count: totalCount,
   items: [],
@@ -25,3 +28,9 @@ const reposList = reposData.map(name => makeFakeRepo({ name, id: name }))
 
 export const getReposListBy = ({ name }) =>
   reposList.filter(repo => repo.name === name)
+
+export const getReposPerPage = ({ currentPage, perPage }) => {
+  return perPage === 30
+    ? repos30Paginated[currentPage]
+    : repos50Paginated[currentPage]
+}
