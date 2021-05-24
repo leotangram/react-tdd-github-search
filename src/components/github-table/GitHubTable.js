032c771505541d2d176760bb6e1da89ab08ecb98
Leonardo Omaña <leotangram@gmail.com>
2021-05-24 15:30:54 -0500

    Update table with sticky header

--- src/components/github-table/GitHubTable.js
+++ src/components/github-table/GitHubTable.js
@@ -1,6 +1,7 @@
 import {
   Avatar,
   Link,
+  makeStyles,
   Table,
   TableBody,
   TableCell,
@@ -18,44 +19,54 @@ const tableHeaders = [
   'Updated at',
 ]
 
-const GitHubTable = ({ repoList }) => (
-  <TableContainer>
-    <Table>
-      <TableHead>
-        <TableRow>
-          {tableHeaders.map(name => (
-            <TableCell key={name}>{name}</TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {repoList.map(
-          ({
-            forks_count: forksCount,
-            id,
-            name,
-            open_issues_count: openIssuesCount,
-            owner: { avatar_url: avatarUrl },
-            stargazers_count: stargazersCount,
-            updated_at: updatedAt,
-            html_url: htmlUrl,
-          }) => (
-            <TableRow key={id}>
-              <TableCell>
-                <Avatar src={avatarUrl} alt={name} />
-                <Link href={htmlUrl}>{name}</Link>
-              </TableCell>
-              <TableCell>{stargazersCount}</TableCell>
-              <TableCell>{forksCount}</TableCell>
-              <TableCell>{openIssuesCount}</TableCell>
-              <TableCell>{updatedAt}</TableCell>
-            </TableRow>
-          ),
-        )}
-      </TableBody>
-    </Table>
-  </TableContainer>
-)
+const useStyles = makeStyles({
+  container: {
+    maxHeight: 440,
+  },
+})
+
+const GitHubTable = ({ repoList }) => {
+  const classes = useStyles()
+
+  return (
+    <TableContainer className={classes.container}>
+      <Table stickyHeader>
+        <TableHead>
+          <TableRow>
+            {tableHeaders.map(name => (
+              <TableCell key={name}>{name}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {repoList.map(
+            ({
+              forks_count: forksCount,
+              id,
+              name,
+              open_issues_count: openIssuesCount,
+              owner: { avatar_url: avatarUrl },
+              stargazers_count: stargazersCount,
+              updated_at: updatedAt,
+              html_url: htmlUrl,
+            }) => (
+              <TableRow key={id}>
+                <TableCell>
+                  <Avatar src={avatarUrl} alt={name} />
+                  <Link href={htmlUrl}>{name}</Link>
+                </TableCell>
+                <TableCell>{stargazersCount}</TableCell>
+                <TableCell>{forksCount}</TableCell>
+                <TableCell>{openIssuesCount}</TableCell>
+                <TableCell>{updatedAt}</TableCell>
+              </TableRow>
+            ),
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
 
 GitHubTable.propTypes = {
   repoList: PropTypes.arrayOf(PropTypes.object).isRequired,
